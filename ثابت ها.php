6202b72764766f541bc5b55090bd53d18302549f
FArshad2020s <farshadarbab2020@gmail.com>
2022-04-05 17:20:57 +0430

    FA

---
+++ ثابت ها.php
@@ -0,0 +1,7 @@
+<?php
+const google_url="https://www.google.com";
+echo google_url."<br>" ;
+#-----------------------------------------
+define("youtube_url","https;//www.youtube.com");
+echo youtube_url ;
+?>
